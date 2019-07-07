05a592e2f0c303f437045a4b401ae79d401bbf44
Darnley Costa <darnleycosta@gmail.com>
2019-07-07 17:39:54 -0300

    Add helmet and body-parser

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(helmet());
 
 app.use(express.static(path.join(__dirname, 'app/dist')));
 
