74afed1841f20c902d25cad363c2019e3407a219
Darnley Costa <darnleycosta@gmail.com>
2019-07-07 18:17:07 -0300

    Add dotenv package

--- index.js
+++ index.js
@@ -2,20 +2,29 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const dotenv = require('dotenv');
 
 const app = express();
+
+dotenv.config();
+
+// Load the HTTP port
+const APPLICATION_PORT = Number(process.argv[2]) || Number(process.env.PORT);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Load security defaults
 app.use(helmet());
 
+// Serve static files from app/dist folder
 app.use(express.static(path.join(__dirname, 'app/dist')));
 
+// Process all other requests to the index.hml file
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'app/dist/index.html'));
 });
 
-app.listen(3000, () => {
-    console.info('Listening application');
+app.listen(APPLICATION_PORT, () => {
+    console.info(`Application started on port ${APPLICATION_PORT}`);
 });
